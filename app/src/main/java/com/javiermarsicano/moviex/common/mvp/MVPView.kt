b74a98823b539e89a049ee8330045e095e14e86b
Javier Marsicano <contact@javiermarsicano.com>
2022-02-14 12:21:31 -0300

    remove mvp components

--- app/src/main/java/com/javiermarsicano/moviex/common/mvp/MVPView.kt
+++
@@ -1,9 +0,0 @@
-package com.javiermarsicano.moviex.common.mvp
-
-interface MVPView {
-    fun showLoading()
-    fun hideLoading()
-    fun onError(resId: Int)
-    fun onError(message: String?)
-    fun hideKeyboard()
-}
